8c642a81fe04f172537ad63348dc3ed79cfc3c0e
Andrew Schorr <afs35mm@gmail.com>
2018-01-25 18:03:36 -0500

    Added splash page

---
+++ resources/views/splash.blade.php
@@ -0,0 +1,10 @@
+@section('pageStyle')
+<!-- <link href="{{ asset('styles/welcome.css') }}" rel="stylesheet"> -->
+@endsection
+@extends('layouts.vita')
+@section('content')
+<p>HELLOOO</p>
+@endsection
+@section('pageScript')
+<!-- <script src="{{ asset('js/welcome.js') }}" defer></script> -->
+@endsection
